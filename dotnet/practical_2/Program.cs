7a3f168ef386c88a7ad5369ffc9c77f741251efe
rajan_majithiya <17comp.rajanmajithiya@gmail.com>
2020-01-20 13:21:18 +0530

    initial commit

---
+++ dotnet/practical_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practical_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            string country;
+
+            Console.WriteLine("Enter your name:");
+            name= Console.ReadLine();
+
+            Console.WriteLine("Enter your Country");
+             country=Console.ReadLine();
+
+            Console.WriteLine("HI" + name + "your country is " + country);
+        
+
+        }
+    }
+}
